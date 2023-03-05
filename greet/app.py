e7f3838ecf4f1cff3814887a645481da725304c9
John Myers <johnmyers@Johns-MacBook-Pro.local>
2023-03-05 08:05:27 -0500

    first half

---
+++ greet/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    return "Welcome"
+
+@app.route('/welcome/honme')
+def welcome_home():
+    return "Welcome Home"
+
+@app.route('/welcome/back')
+def welcome_back():
+    return "Welcome back"
